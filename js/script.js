b205edd651aea2c551a9f331562c2b679396c3b2
pavlo20031 <lesivpavlo999@gmail.com>
2026-06-13 21:28:02 +0300

    commit67

---
+++ js/script.js
@@ -0,0 +1,135 @@
+const listRef = document.querySelector(".js-gallery");
+const modalRef = document.querySelector(".js-lightbox");
+const backdropRef = document.querySelector(".lightbox__overlay");
+const closeRef = document.querySelector('[data-action="close-lightbox"]');
+const modalImageRef = document.querySelector(".lightbox__image");
+const galleryItems = [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
+const markup = galleryItems
+  .map(
+    (itm) => `
+  <li class="gallery__item">
+      <img
+        class="gallery__image"
+        src="${itm.preview}"
+        data-source="${itm.original}"
+        alt="${itm.description}"
+      />
+  </li>
+`,
+  )
+  .join("");
+
+listRef.innerHTML = markup;
+
+const openModal = (src, alt) => {
+  modalRef.classList.add("is-open");
+  modalImageRef.src = src;
+  modalImageRef.alt = alt;
+};
+
+const closeModal = () => {
+  modalRef.classList.remove("is-open");
+  modalImageRef.src = "";
+  modalImageRef.alt = "";
+};
+
+closeRef.addEventListener("click", closeModal);
+
+listRef.addEventListener("click", (evt) => {
+  evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") { return };
+
+  const largeImageUrl = evt.target.dataset.source; 
+  const description = evt.target.alt;
+
+    openModal(largeImageUrl, description);
+    
+});
+let currentIndex = 0;
+document.addEventListener("keydown", (evt) => {
+  if (!modalRef.classList.contains("is-open")) return;
+
+  if (evt.code === "Escape") {
+    closeModal();
+  }
+
+  if (evt.code === "ArrowRight") {
+    currentIndex = (currentIndex + 1) % galleryItems.length;
+    modalImageRef.src = galleryItems[currentIndex].original;
+    modalImageRef.alt = galleryItems[currentIndex].description;
+  }
+
+  if (evt.code === "ArrowLeft") {
+    currentIndex =
+      (currentIndex - 1 + galleryItems.length) % galleryItems.length;
+    modalImageRef.src = galleryItems[currentIndex].original;
+    modalImageRef.alt = galleryItems[currentIndex].description;
+  }
+});
+backdropRef.addEventListener("click", (evt) => {
+    closeModal();
+});
